ff7ae28797c9fe991ee5360ad5e4d55247b04dec
dev/linux <dev@gmail.com>
2022-11-07 18:21:24 +0530

    commit

---
+++ Incaendo_Client/src/api/index.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+const url = 'http://3.110.77.190:5000/posts';
+
+export const fetchPosts = () => axios.get(url);
+
+export const createPost = (newPost) => axios.post(url, newPost);
+
+export const updatePost = (id, updatedPost) => axios.patch(`${url}/${id}`, updatedPost);
