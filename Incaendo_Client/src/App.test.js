2c453c745359fdf94c56ae763dcc4fc5754073f0
Rajat/linux <rajat@gmail.com>
2022-11-29 11:50:21 +0530

    commit

--- Incaendo_Client/src/App.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import React from "react";
-import App from "./index";
-
-test("renders app title element", () => {
-  render(<App />);
-  const titleElement = screen.getByText(/Memories/i);
-  expect(titleElement).toBeInTheDocument();
-});
--- Incaendo_Client/src/api/index.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const url = 'http://198.11.174.204:5000/posts';
-
-export const fetchPosts = () => axios.get(url);
-
-export const createPost = (newPost) => axios.post(url, newPost);
-
-export const updatePost = (id, updatedPost) => axios.patch(`${url}/${id}`, updatedPost);
